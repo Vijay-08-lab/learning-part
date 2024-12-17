a4b092e5eb192e69a3696fd4c76017490db6e479
vijay-08-lab <vijaykrishna.venkatesan@gmail.com>
2024-12-17 21:53:59 +0530

    modified

--- learn.js
+++ learn.js
@@ -24,7 +24,7 @@ let text = ''
      
  }
  console.log(text)
- */
+ 
 // task create a function and make the string value 'vijay' in properCase ?
 let name = 'GOOGLE'; // first i am declearing a string value in a variable as name in uppercase.
 function getInProperCase(name){ // next i am creating a function called getpropercase and assing a parameter inside the function
@@ -37,3 +37,27 @@ const properCase = (ids) => { // so here is am using arrow fucntion you can use 
     return ids.charAt(0).toUpperCase() + ids.slice(1).toLowerCase()
 }
 console.log(properCase(ids))
+*/
+// Array //
+
+// task is to get the last index of array value to be print
+const myArray = [];
+myArray[0] = 'Banana';
+myArray[1] = 200;
+myArray[2] = true;
+
+console.log(myArray.length) // i print the total array length.
+console.log(myArray[1]) // it print the value in the 200 which is stored in the index value of array 1
+console.log(myArray[myArray.length -1]) // by subracting the length value with the one you can able to get the last because the array will always calculate the index value +1.
+console.log(myArray)
+ myArray.push('vijay') // adding an element in last by using push() method.
+ console.log(myArray)
+ myArray.pop() // now i removed the last element using pop method.
+ console.log(myArray)
+ myArray.unshift('age') // it adds a new element in first index of array
+ console.log(myArray)
+myArray.shift() // now i am deleteing the first element
+console.log(myArray)
+myArray.splice(1,1); // so using splice method i deleted the value 200. splice method ( starting index number,deleting count)
+console.log(myArray)
+
