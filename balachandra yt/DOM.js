582ff5da5645444ec2780098cd5f0d8a340a7a10
vijay-08-lab <vijaykrishna.venkatesan@gmail.com>
2025-01-06 18:46:16 +0530

    modified

--- balachandra yt/DOM.js
+++ balachandra yt/DOM.js
@@ -10,4 +10,18 @@ const gview = document.querySelectorAll(".view")
 console.log(gview)
 const divs = view1.querySelectorAll("div")
 console.log(divs)
- 
+const sameview =view1.getElementsByTagName("div")
+console.log(sameview)
+const evendivs = view1.querySelectorAll("div:nth-of-type(2n)")
+console.log(evendivs);
+evendivs[1].style.backgroundColor = "purple";
+for (let i =0; i<evendivs.length;i++){
+    evendivs[i].style.width = "150px";
+    evendivs[i].style.backgroundColor = "green";
+}
+const navText = document.querySelector("nav h1")
+console.log(navText)
+navText.textContent = "!Hello World";
+navText.innerHTML = "<div>inserted</div>"
+console.log(evendivs[0])
+console.log(evendivs[0].parentElement)
