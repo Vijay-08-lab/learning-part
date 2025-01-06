c7a710797f0b4f8ff470457297e6f67cf635921c
vijay-08-lab <vijaykrishna.venkatesan@gmail.com>
2025-01-06 13:26:44 +0530

    modified

---
+++ balachandra yt/DOM.js
@@ -0,0 +1,13 @@
+const view1 = document.getElementById("view1")
+console.log(view1);
+const view2 = document.querySelector("#view2")
+console.log(view2);
+view1.style.display ='flex';
+view2.style.display = 'none';
+const nview = document.getElementsByClassName("view")
+console.log(nview)
+const gview = document.querySelectorAll(".view")
+console.log(gview)
+const divs = view1.querySelectorAll("div")
+console.log(divs)
+ 
