84b4a3f37d1ca4275243cbf4499f3f0aeaefe9d7
vijay-08-lab <vijaykrishna.venkatesan@gmail.com>
2025-01-09 16:30:58 +0530

    tailwind basic over

--- tailwindcss/tailwindcss-basic/tailwind.config.js
+++ tailwindcss/tailwindcss-basic/tailwind.config.js
@@ -2,11 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: { // in extends you can add customs options you need first classified under extends and need to give the data in sting method (i.e) in qutotes.
+      colors: {
+        'vijay-purple': '#009688'
+      },
+      fontWeight: {
+        'vijay-weight': "200"
+      }
+    },
     fontFamily: {
       bebas:['Bebas Neue','sans-serif']
     }
   },
   plugins: [],
+  darkMode:"selector"
 }
 
