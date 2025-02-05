44815a6d64a6448d66fb272a781ac2ced23167e5
vijay-08-lab <vijaykrishna.venkatesan@gmail.com>
2025-02-05 17:34:15 +0530

    modofied

--- balachandra yt/module/main.js
+++ balachandra yt/module/main.js
@@ -2,6 +2,7 @@
 //  import myfun from "./working.js";
 //  import { nextfun } from "./working.js";
 import * as dh from "./working.js";
+import { newObj } from "./working.js";
 import { subscriber } from "./working.js";
  console.log(dh.default())
 console.log(dh.nextfun())
@@ -9,3 +10,18 @@ const me = new subscriber("vijay")
 console.log(me.email())
 const anotheruser = new subscriber("Raghu")
 console.log(anotheruser.email())
+
+//forEach is a method which is used to get all the data from the Json file. for data from the working file to main. it act's like a loop.
+// newObj.forEach((newObj)=>{
+//     console.log(newObj)
+// });
+//console.clear()//
+const filtterPost = newObj.filter((newObj) => {
+return newObj.userId === 2;
+})
+console.log(filtterPost)
+
+const mapsfilter = filtterPost.map((newObj)=>{
+    return newObj.id;
+})
+console.log(mapsfilter)
--- balachandra yt/module/working.js
+++ balachandra yt/module/working.js
@@ -14,5 +14,31 @@ export class subscriber {
         return `${this.name} dear user you should mail this dooo@gmail.com`
     }
 }
+const newObj = [
+    {
+"userId": 1,
+"id":1,
+"title": 'eyes',
+"body": "eyes are the important organ in the human part that helps the human to identified the think and realise the color ana feel of light",
+    },
+    {
+"userId":1,
+'id':2,
+"title": 'nose',
+"body": "nose is also a one of the important part of human part it used to smell the thinks that human can see and feel", 
+    },
+{      
+"userId":2,
+'id': 1,
+"title": 'heart',
+"body": "heart is the one of the important oran in the human part. the heart pumps the blood and that cirulates the entire body oragan s to work. when the heart got failuer the human will leads to die.",
+},
+{
+"userId" : 2,
+'id' : 2,
+'title': "Brain",
+"body" : " brain is the one of the main organ in the human body. the function of the brain is more important beacause the in human the brain the organ which helps the human to think feel smell the think. the brain is like a main think that will helps the other organs to do their jobs",
+},
+]
 export default myfun;
-export {nextfun}
+export {nextfun,newObj}
