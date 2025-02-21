4fa070ec183e1607f18e1a2286a789b08ece2750
vijay-08-lab <vijaykrishna.venkatesan@gmail.com>
2025-02-21 13:55:50 +0530

    modified

--- React/learnapp/src/Content.js
+++ React/learnapp/src/Content.js
@@ -9,9 +9,12 @@ return mood[randomchange]
 const handelClick = (name) => {
 console.log(`thanks for clicking me! ${name}`)
 }
+const handelClick2 = () => {
+    console.log(`you have clicked DoubleClick`)
+}
 return (
     <div>
-    <p>I am in {mindchange()}</p>
+    <p onDoubleClick={handelClick2}>I am in {mindchange()}</p>
     <button onClick={()=>{handelClick('vijay')}}>Click me!</button>
     </div>
 )
